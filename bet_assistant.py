ac0f91ccd1ee42a87f5fdcf182785e449ecdfc10
Rotaru Razvan <rotarurazvan07@gmail.com>
2022-08-28 22:50:40 +0300

    Go to the all matches page and save the html of all fixtures

--- bet_assistant.py
+++ bet_assistant.py
@@ -1,8 +1,10 @@
 """
 Bet assistant, 2022
 """
-from selenium import webdriver
+import time
 
+from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.service import Service
 
@@ -26,7 +28,23 @@ def init_driver(chrome_path, chromedriver_path):
     return driver
 
 
+def get_all_matches_html(url, driver):
+    driver.get(url)
+    time.sleep(5)
+
+    # Press the "Show more" button at the bottom of the page by running the script it is executing
+    for i in range(11, 30):
+        driver.execute_script("ltodrows(\"1x2\"," + str(i) + ",\"\");")
+        time.sleep(1)
+
+    html = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.close()
+
+    return html
+
+
 if __name__ == "__main__":
     browser_driver = init_driver(CHROME_PATH, CHROMEDRIVER_PATH)
     if browser_driver is None:
         quit()
+    matches_html = get_all_matches_html(FOREBET_ALL_PREDICTIONS_URL, browser_driver)
